4d6037b446b15da509248f777466155cacdb291e
Bang0123 <nikolajbang1995@gmail.com>
2017-03-11 21:16:29 +0100

    One more metric shown

--- MasterCrack/Master.cs
+++ MasterCrack/Master.cs
@@ -114,6 +114,7 @@ namespace MasterCrack
 
         private void PrintTimeElapsed()
         {
+            Console.WriteLine("Hashes computed and tried: " + Hashestried);
             Console.WriteLine("Total time used by clients added together: " + ClientsWorkTimeSpan);
             Console.WriteLine("Total time Elapsed: " + TotalTimeWatch?.Elapsed);
         }
